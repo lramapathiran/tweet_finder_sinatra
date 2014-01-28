724bcc5ac9acbf4299090e4e10ee4c85f1551525
lramapathiran <lramapathiran@gmail.com>
2014-01-29 00:16:41 +0100

    app.rb

---
+++ app.rb
@@ -0,0 +1,20 @@
+require 'bundler'
+require "sinatra"
+bundler.require
+
+client = Twitter::REST::Client.new do |config|
+  config.consumer_key        = "cQVZeos4bAmXTb5HnPROlw"
+  config.consumer_secret     = "UMBZ2ds0lSTKyLygddiPXlxqXGSxOyostSPSfsP2KI"
+  config.access_token        = "182766751-w2eWOZnSGZrAkLo0fVp7in69QiaDgGcPCmdwOhOJ"
+  config.access_token_secret = "Wo7OIxEsUl5Hu9omCBTRPOu2LDFmbWVyd0Wopqti4deFc"
+end
+
+get '/' do
+	@tweets = client.search('foo')	
+	erb '
+	<div>
+	<% @tweets.each do |tweet| %>
+		<p> <%= tweet.text %> </p>
+	<% end %> 
+	</div>'
+end
